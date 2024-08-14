f13c93e519be6cafd17e49c3c6d9eae9b99d7cb3
Hans Villaizan <zolymo@gmail.com>
2024-08-13 23:35:48 -0500

    consumer example

--- src/main/java/com/brixton/functional/consumer/ConsumerExample.java
+++ src/main/java/com/brixton/functional/consumer/ConsumerExample.java
@@ -1,15 +1,31 @@
 package com.brixton.functional.consumer;
 
+import com.brixton.functional.lambdas.Persona;
+import com.brixton.functional.repo.Person;
+
 import java.util.function.Consumer;
 
 public class ConsumerExample {
 
 	public static void main(String[] args) {
 		Consumer<String> con1 = (s) -> {System.out.println(s.toUpperCase());};
+		Consumer<String> con2 = (s) -> {System.out.println(s.toLowerCase());};
+		Consumer<String> con3 = (s) -> {System.out.println(s.length());};
+
+		//con1.accept("hans");
+
+		//con2.accept("YOSELIN");
+
+		con1.andThen(con2).accept("hans");
+		con1.andThen(con3).andThen(con2).accept("YoSeLin");
+
+		Persona x = new Persona();
+		x.procesaLambdas("EduarDO");
+
 		//con1.accept("java8");
-		Consumer<String> con2 = (s) -> System.out.println(s.toLowerCase());
+		//Consumer<String> con2 = (s) -> System.out.println(s.toLowerCase());
 		//con2.accept("java8");
-		con1.andThen(con2).accept("java8");
+		//con1.andThen(con2).accept("java8");
 	}
 
 }
--- src/main/java/com/brixton/functional/lambdas/Persona.java
+++ src/main/java/com/brixton/functional/lambdas/Persona.java
@@ -5,6 +5,7 @@ import lombok.Setter;
 
 import java.time.LocalDate;
 import java.time.Period;
+import java.util.function.Consumer;
 import java.util.function.Function;
 
 @Setter
@@ -15,6 +16,10 @@ public class Persona {
     String nombre;
     String apellidos;
     LocalDate fechaNacimiento;
+
+    Consumer<String> con1 = (s) -> {System.out.println(s.toUpperCase());};
+    Consumer<String> con2 = (s) -> {System.out.println(s.toLowerCase());};
+    Consumer<String> con3 = (s) -> {System.out.println(s.length());};
 
     Function<LocalDate, Integer> calculaEdad = (fnac) -> { LocalDate fechaActual = LocalDate.now();
         Period periodo = Period.between(fnac, fechaActual);
@@ -34,6 +39,10 @@ public class Persona {
         LocalDate fechaActual = LocalDate.now();
         Period periodo = Period.between(fechaNacimiento, fechaActual);
         return periodo.getYears();
+    }
+
+    public void procesaLambdas(String input){
+        con1.andThen(con3).andThen(con2).accept(input);
     }
 
 }
